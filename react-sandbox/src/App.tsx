0b24a2be6b81a1e3d2292453952f0228792a5bdb
SebC750 <SebC89999@gmail.com>
2024-12-29 12:08:58 -0500

    Test React TS project

---
+++ react-sandbox/src/App.tsx
@@ -0,0 +1,13 @@
+'use client'
+import './App.css'
+import {Button} from "./Button.tsx"
+function App() {
+  
+  return (
+    <>
+      <Button type="submit" autoFocus={true} defaultValue="test" className="bg-blue-400"> </Button>
+    </>
+  )
+}
+
+export default App
